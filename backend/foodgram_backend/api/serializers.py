731f61ad4c0098c241dd82d52be87f50809fa8e7
Иван Шелегов <ivan.shelegov@yandex.ru>
2023-05-24 14:32:24 +0300

    v3

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -1,8 +1,11 @@
 import base64
 
+from django.conf import settings
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import check_password, make_password
 from django.core.files.base import ContentFile
 from django.shortcuts import get_object_or_404
-from rest_framework import exceptions, serializers
+from rest_framework import exceptions, serializers, validators
 
 from recipes.models import (
     Ingredient,
@@ -11,7 +14,7 @@ from recipes.models import (
     RecipeUser,
     Tag
 )
-from users.serializers import UserSerializer
+from users.models import Subscription, User
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -38,13 +41,104 @@ class RecipeIngredientSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'measurement_unit', 'amount')
 
 
+class UserSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+            'is_subscribed'
+        )
+        model = User
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=User.objects.all(),
+                fields=('username', 'email')
+            )
+        ]
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        user = request.user if request else None
+        if user and user.is_authenticated:
+            return obj.subscribers.filter(id=user.id).exists()
+        return False
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data.pop('password', None)
+        request = self.context.get('request')
+        if request and request.method == 'POST':
+            data.pop('is_subscribed')
+        return data
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(
+            validated_data.get('password')
+        )
+        return super().create(validated_data)
+
+
+class TokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        max_length=settings.EMAIL_CHAR_LENGTH
+    )
+    password = serializers.CharField(
+        max_length=settings.PASSWORD_LENGTH
+    )
+
+    def validate(self, data):
+        user = get_object_or_404(
+            User,
+            email=data['email']
+        )
+        if not check_password(data.get('password'), user.password):
+            raise serializers.ValidationError('Неверный email или пароль!')
+        return data
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    new_password = serializers.CharField(
+        max_length=settings.PASSWORD_LENGTH, write_only=True
+    )
+    current_password = serializers.CharField(
+        max_length=settings.PASSWORD_LENGTH, write_only=True
+    )
+
+    def validate(self, data):
+        request = self.context.get('request')
+        user = authenticate(
+            request=request,
+            username=request.user.username,
+            password=data['current_password']
+        )
+        if not user:
+            raise serializers.ValidationError('Invalid current password.')
+        return data
+
+
+class SubscribeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ('author', 'subscriber')
+        read_only_fields = ('author', 'subscriber')
+
+    def to_representation(self, instance):
+        return SubscriptionSerializer(instance, context=self.context).data
+
+
 class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
         if isinstance(data, str) and data.startswith('data:image'):
-            format, imgagestring = data.split(';base64,')
+            format, imgage_string = data.split(';base64,')
             ext = format.split('/')[-1]
             data = ContentFile(
-                base64.b64decode(imgagestring), name=f'temp.{ext}'
+                base64.b64decode(imgage_string), name=f'temp.{ext}'
             )
         return super().to_internal_value(data)
 
@@ -87,7 +181,7 @@ class RecipeWriteSerializer(RecipeBaseSerializer):
         for ingredient_data in ingredients_data:
             ingredient_id = ingredient_data.pop('id')
             amount = ingredient_data.pop('amount')
-            ingredient = Ingredient.objects.get(id=ingredient_id)
+            ingredient = get_object_or_404(Ingredient, id=ingredient_id)
             RecipeIngredient.objects.create(
                 recipe=recipe,
                 ingredient=ingredient,
@@ -156,6 +250,45 @@ class RecipeReadSerializer(RecipeBaseSerializer):
             return obj.recipe_user.get(user=user).is_in_shopping_cart
         except RecipeUser.DoesNotExist:
             return False
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    email = serializers.ReadOnlyField(source='author.email')
+    id = serializers.ReadOnlyField(source='author.id')
+    username = serializers.ReadOnlyField(source='author.username')
+    first_name = serializers.ReadOnlyField(source='author.first_name')
+    last_name = serializers.ReadOnlyField(source='author.last_name')
+    is_subscribed = serializers.SerializerMethodField()
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.ReadOnlyField(source='author.recipe.count')
+
+    class Meta:
+        model = Subscription
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+            'recipes',
+            'recipes_count'
+        )
+
+    def get_is_subscribed(self, obj):
+        if obj:
+            return True
+
+    def get_recipes(self, obj):
+        limit = self.context.get('request').GET.get('recipes_limit')
+        recipe_obj = obj.author.recipe.all()
+        if limit:
+            recipe_obj = recipe_obj[:int(limit)]
+        from api.serializers import ShortRecipeSerializer
+        serializer = ShortRecipeSerializer(
+            recipe_obj, many=True, context=self.context
+        )
+        return serializer.data
 
 
 class IsFavoritedSerializer(serializers.ModelSerializer):
--- backend/foodgram_backend/users/views.py
+++ backend/foodgram_backend/users/views.py
@@ -11,7 +11,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
 from .models import Subscription, User
-from .serializers import (
+from api.serializers import (
     ChangePasswordSerializer,
     SubscribeSerializer,
     SubscriptionSerializer,
